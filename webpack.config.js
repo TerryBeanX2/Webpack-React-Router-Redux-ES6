3de71f56395807a1a95d5d81649d45ffb8cb6d8f
袁阳 <yuanyang@wecash.net>
2018-03-08 14:08:35 +0800

    分离

--- webpack.config.js
+++ webpack.config.js
@@ -102,33 +102,25 @@ let conf = {
     },
 };
 
-let prodConf;
+
 
 if (process.env.NODE_ENV) {
-    prodConf = {
-        devtool: 'cheap-source-map',
-        plugins: [
-            new webpack.optimize.CommonsChunkPlugin({
-                name: 'vendor',
-                minChunks(module) {
-                    // any required modules inside node_modules are extracted to vendor
-                    if (module.resource) console.log(module.resource)
-                    return (module.resource &&
-                    /\.js$/.test(module.resource) &&
-                    module.resource.indexOf(
-                        path.join(__dirname, './node_modules')
-                    ) === 0);
-                }
-            }),
-            // extract webpack runtime and module manifest to its own file in order to
-            // prevent vendor hash from being updated whenever app bundle is updated
-            new webpack.optimize.CommonsChunkPlugin({
-                name: 'manifest',
-                minChunks: Infinity
-            })
-        ]
-    }
-    conf = Object.assign({},conf,prodConf)
+    conf.devtool =  'cheap-source-map';
+    conf.plugins.push(new webpack.optimize.CommonsChunkPlugin({
+        name: 'vendor',
+        minChunks(module) {
+            // any required modules inside node_modules are extracted to vendor
+            return (module.resource &&
+            /\.js$/.test(module.resource) &&
+            module.resource.indexOf(
+                path.join(__dirname, './node_modules')
+            ) === 0);
+        }
+    }));
+    conf.plugins.push(new webpack.optimize.CommonsChunkPlugin({
+        name: 'manifest',
+        minChunks: Infinity
+    }));
 }
 
 module.exports = conf;
